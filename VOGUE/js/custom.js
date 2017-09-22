31840e2838234c48cfb55a0e3ccec01e731c48ab
soggii <lsy9108@gmail.com>
2017-09-22 19:23:10 +0900

    VOGUE
    add Vogue website

---
+++ VOGUE/js/custom.js
@@ -0,0 +1,45 @@
+﻿$(document).ready(function(){
+    
+    /* when click .btnMenu */
+    $('.btnMenu').on('click',function(){
+        $(this).fadeOut();
+        $('section').addClass('on');
+        $('nav').addClass('on');
+    });
+    
+    /* when click category menu */
+    $('.nav li').on('click',function(){
+        $('.btnMenu').fadeIn();
+        $('section').removeClass('on');
+        $('nav').removeClass('on');
+    });
+    
+    
+    /* when click button in nav */
+    $('nav li').on('click',function(){
+        $('.btnMenu').fadeIn();
+        $('section').removeClass('on');
+        $('nav').removeClass('on');
+        
+        var i = $(this).index();
+        $('section>div').removeClass('on');
+        $('section>div').eq(i).addClass('on');
+    });
+    
+    
+}); // the end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
